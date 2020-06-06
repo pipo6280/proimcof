17959fbe9653b5983f45c7a9c6309d42b2827afd
pipo6280 <pipo6280@gmail.com>
2020-06-06 14:41:58 -0500

    ajuste db real

--- app/config/database.php
+++ app/config/database.php
@@ -1,8 +1,8 @@
 <?php
 return array(
     'hostname' => 'localhost',
-    'username' => 'root', // imprecof_fact
-    'password' => "", // &qE[=ufIHyAM
+    'username' => 'imprecof_fact', // imprecof_fact
+    'password' => "&qE[=ufIHyAM", // 
     'database' => 'imprecof_fact',
     'dbdriver' => 'pdo_mysql',
     'charset' => 'utf8',
